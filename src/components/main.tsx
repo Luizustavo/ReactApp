7e74c0b3b14b33a11c383067657db1f1de9e712d
Luizustavo <luizg.ss@outlook.com>
2024-09-21 01:16:05 -0300

    est

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -11,9 +11,10 @@ function Main() {
         qualidade, segurança e eficiência, fornecendo produtos que ajudam nossos
         clientes a alcançar o sucesso.
       </p>
-
+      <br />
+      <br />
       <section>
-        <h3>Nossos Serviços</h3>
+        <h2>Nossos Serviços</h2>
         <p>
           Oferecemos uma variedade de serviços para apoiar seu negócio,
           incluindo:
@@ -24,6 +25,7 @@ function Main() {
           <li>Integração de sistemas de pagamento</li>
           <li>Automação de processos empresariais</li>
         </ul>
+        <br /><br />
 
         {/* Carrossel de Imagens */}
         <div className="carousel">
@@ -36,15 +38,24 @@ function Main() {
             showStatus={false}
           >
             <div>
-              <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg" alt="Serviço 1" />
+              <img
+                src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg"
+                alt="Serviço 1"
+              />
               <p className="legend">Desenvolvimento de Software</p>
             </div>
             <div>
-              <img src="https://images.pexels.com/photos/5582873/pexels-photo-5582873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Serviço 2" />
+              <img
+                src="https://images.pexels.com/photos/5582873/pexels-photo-5582873.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="Serviço 2"
+              />
               <p className="legend">Consultoria em TI</p>
             </div>
             <div>
-              <img src="https://images.pexels.com/photos/5632402/pexels-photo-5632402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Serviço 3" />
+              <img
+                src="https://images.pexels.com/photos/5632402/pexels-photo-5632402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt="Serviço 3"
+              />
               <p className="legend">Integração de Pagamentos</p>
             </div>
           </Carousel>
